a4c8de401a96d2b81fed7cc0e393b75ec33c0e7a
Ryan Webster <ryan.h.webster@gmail.com>
2020-09-04 23:16:38 -0700

    making computer play

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,9 @@ let player = "X";
 let squareValues = ["", "", "", "", "", "", "", "", ""];
 let winner = "";
 let stopProp = false;
+const board = document.getElementById(`tic-tac-toe-board`);
+let giveUpButton = document.getElementById('give-up')
+giveUpButton.disabled = false
 
 function saveGame() {
   window.localStorage.setItem("squareValues", squareValues);
@@ -9,7 +12,99 @@ function saveGame() {
   window.localStorage.setItem("player", player);
 }
 
+function getRandomInt(n) {
+  return Math.floor(Math.random() * Math.floor(n))
+}
+
+const checkWinner = (squareValues) => {
+  for (let i = 0; i < squareValues.length; i += 3) {
+    // rows
+    const el1 = squareValues[i];
+    const el2 = squareValues[i + 1];
+    const el3 = squareValues[i + 2];
+    if (el1 !== "" && el1 === el2 && el2 === el3) {
+      winner = `Winner: ${el1}`;
+      giveUpButton.disabled = true;
+      stopProp = true;
+
+      return true
+    }
+  }
+  for (let i = 0; i < 3; i += 1) {
+    // columns
+    const el1 = squareValues[i];
+    const el2 = squareValues[i + 3];
+    const el3 = squareValues[i + 6];
+    if (el1 !== "" && el1 === el2 && el2 === el3) {
+      winner = `Winner: ${el1}`;
+      giveUpButton.disabled = true;
+      stopProp = true;
+      return true
+    }
+  }
+  for (let i = 0; i < 3; i += 1) {
+    // columns
+    const el1 = squareValues[i];
+    const el2 = squareValues[i + 3];
+    const el3 = squareValues[i + 6];
+    if (el1 !== "" && el1 === el2 && el2 === el3) {
+      winner = `Winner: ${el1}`;
+      giveUpButton.disabled = true;
+      stopProp = true;
+      return true
+    }
+  }
+  if (
+    squareValues[4] !== "" &&
+    ((squareValues[0] === squareValues[4] &&
+      squareValues[4] === squareValues[8]) ||
+      (squareValues[2] === squareValues[4] &&
+        squareValues[4] === squareValues[6]))
+  ) {
+    winner = `Winner: ${squareValues[4]}`;
+    giveUpButton.disabled = true;
+    stopProp = true;
+    console.log(stopProp);
+  }
+  if (!squareValues.includes("") && winner === "") {
+    winner = "Winner: None! Tied you losers!";
+    giveUpButton.disabled = true;
+  }
+  document.getElementById("game-status").innerHTML = winner;
+};
+
+function computerPlays() {
+  let computerSpace = getRandomInt(8)
+  
+  
+
+  if (squareValues[computerSpace] === '') {
+    const img = document.createElement("img");
+    img.src = `https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${player.toLowerCase()}.svg`;
+    document.getElementById(`square-${computerSpace}`).appendChild(img);
+    squareValues[computerSpace] = player
+    
+    if (player === "X") {
+      player = "O";
+    } else {
+      player = "X";
+    }
+
+    checkWinner(squareValues)
+    document.getElementById("game-status").innerHTML = winner;
+    giveUpButton.disabled = true;
+    return;
+  } else {
+    computerPlays();
+  }
+}
+
 window.addEventListener("DOMContentLoaded", (event) => {
+  const isEmpty = (squareValues) => squareValues === ''
+  if (Math.random() >= .5 && squareValues.every(isEmpty)) {
+    setTimeout(computerPlays, 500)
+  }
+
   if (window.localStorage.getItem("player")) {
     squareValues = window.localStorage.getItem("squareValues").split(",");
     player = window.localStorage.getItem("player");
@@ -24,55 +119,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
     document.getElementById("game-status").innerHTML = winner;
   }
-  const board = document.getElementById(`tic-tac-toe-board`);
-  const checkWinner = (squareValues) => {
-    for (let i = 0; i < squareValues.length; i += 3) {
-      // rows
-      const el1 = squareValues[i];
-      const el2 = squareValues[i + 1];
-      const el3 = squareValues[i + 2];
-      if (el1 !== "" && el1 === el2 && el2 === el3) {
-        winner = `Winner: ${el1}`;
-        stopProp = true;
-      }
-    }
-    for (let i = 0; i < 3; i += 1) {
-      // columns
-      const el1 = squareValues[i];
-      const el2 = squareValues[i + 3];
-      const el3 = squareValues[i + 6];
-      if (el1 !== "" && el1 === el2 && el2 === el3) {
-        winner = `Winner: ${el1}`;
-        stopProp = true;
-      }
-    }
-    for (let i = 0; i < 3; i += 1) {
-      // columns
-      const el1 = squareValues[i];
-      const el2 = squareValues[i + 3];
-      const el3 = squareValues[i + 6];
-      if (el1 !== "" && el1 === el2 && el2 === el3) {
-        winner = `Winner: ${el1}`;
-        stopProp = true;
-      }
-    }
-    if (
-      squareValues[4] !== "" &&
-      ((squareValues[0] === squareValues[4] &&
-        squareValues[4] === squareValues[8]) ||
-        (squareValues[2] === squareValues[4] &&
-          squareValues[4] === squareValues[6]))
-    ) {
-      winner = `Winner: ${squareValues[4]}`;
-      stopProp = true;
-      console.log(stopProp);
-    }
-    if (!squareValues.includes("") && winner === "") {
-      winner = "None! Tied you losers!";
-    }
-    document.getElementById("game-status").innerHTML = winner;
-  };
 
+  
+ 
   board.addEventListener("click", (event) => {
     if (stopProp) {
       return;
@@ -97,11 +146,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
       } else {
         player = "X";
       }
+
+      if (event.target.id.startsWith('square-') && winner === '') {
+        setTimeout(computerPlays, 1000)
+      }
       checkWinner(squareValues);
-      saveGame();
+      setTimeout(saveGame, 1001);
+      
     }
   });
 });
+
 document.querySelector(".actions").addEventListener("click", (event) => {
   if (event.target.innerText === "New Game" && winner !== "") {
     localStorage.clear();
@@ -119,4 +174,7 @@ document.querySelector(".actions").addEventListener("click", (event) => {
       }
     });
   }
+  setTimeout(computerPlays, 500)
+
+
 });
